09edca8f54d69367b9652ffd0d772fa64e91bcbb
Mathew Bushell <80981032+MatBushell@users.noreply.github.com>
2026-09-22 10:55:54 -0400

    Add content and styling to content tab.
    Multi-line description of commit,
    feel free to be detailed.

---
+++ src/contactTab.js
@@ -0,0 +1,25 @@
+export function contactTab() {
+    const content = document.querySelector(".content");
+    content.classList.add(".content");
+    const h1 = document.createElement("h1");
+    h1.textContent = "Nana's Kitchen";
+
+    const contactInfo = document.createElement("ul");
+    contactInfo.textContent = "Contact Info: ";
+    contactInfo.classList.add("contactList");
+
+    const address = document.createElement("li");
+    address.textContent = "Address: 742 Heritage Way, Moncton, NB E1C 8R3";
+    address.classList.add("contactItem");
+    
+    const phone = document.createElement("li");
+    phone.textContent = "Phone: (506) 555-0148";    
+
+    const email = document.createElement("li");
+    email.textContent = "Email: info@nanaskitchenacadian.ca";
+
+    const fax = document.createElement("li");
+    fax.textContent = "Fax: (506) 555-0149";
+    contactInfo.append(address, phone, email, fax);
+    content.append(h1, contactInfo);
+};
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import "./reset.css";
 import "./styles.css";
 import {homeTab} from "./homeTab.js";
 import {menuTab} from "./menuTab.js";
+import {contactTab} from "./contactTab.js";
 
 
 
 // homeTab();
-menuTab();
+// menuTab();
+contactTab();
