f82b342c4bd8f28211e8ec731eafbcda06e37491
Mathew Bushell <80981032+MatBushell@users.noreply.github.com>
2026-09-23 14:01:58 -0400

    Add split webpack config files to common, dev, prod + npm scripts
    Multi-line description of commit,
    feel free to be detailed.

---
+++ webpack.prod.js
@@ -0,0 +1,12 @@
+// webpack.prod.js
+import { merge } from "webpack-merge";
+import common from "./webpack.common.js";
+
+export default merge(common, {
+    mode: "production",
+    devtool: "source-map",
+    output: {
+        filename: "[name].[contenthash].js",
+    },
+
+});
